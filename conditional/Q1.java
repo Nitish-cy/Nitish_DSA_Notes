5ac465d6e72f7b22b12c2123fc00efef3459486a
Nitish Mishra <Nitish-cy>
2024-11-28 08:09:35 +0530

    Java Conditionals and Loops  questions

---
+++ conditional/Q1.java
@@ -0,0 +1,15 @@
+// Question1: Write a Java program to get a number from the user and print whether it is
+//  positive or negative
+
+import java.util.*;
+ public class Q1 {
+ public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int x = sc.nextInt();
+    if (x > 0) {
+        System.out.println("x is greater than 0");
+    }else {
+        System.out.println("x is less than or equal 0");
+     }
+    }
+ }
