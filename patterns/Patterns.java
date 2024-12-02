3fad904b181b323eca86ec1cea233d1cfdd78c82
Nitish Mishra <Nitish-cy>
2024-12-02 17:27:44 +0530

    pattern questions added

---
+++ patterns/Patterns.java
@@ -0,0 +1,12 @@
+// try to practice multiple patterns number , char and star patterns 
+1-HOLLOW RECTANGLE pattern
+2-INVERTED & ROTATED HALF-PYRAMID pattern
+3-INVERTED HALF-PYRAMID with Numbers pattern
+4-FLOYD'S Triangle pattern
+5-0-1 Triangle pattern
+6- BUTTERFLY pattern
+7- SOLID RHOMBUS pattern
+8- HOLLOW RHOMBUS pattern
+9- DIAMOND pattern
+10-NUMBER PYRAMID pattern
+11- PALINDROMIC Pattern with Numbers pattern
