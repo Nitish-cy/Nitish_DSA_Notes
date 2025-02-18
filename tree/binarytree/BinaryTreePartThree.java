46cea23b543715f365c8e99dfb21387d1da422ff
Nitish Mishra <Nitish-cy>
2025-02-18 23:45:24 +0530

    tree part three third question done

--- tree/binarytree/BinaryTreePartThree.java
+++ tree/binarytree/BinaryTreePartThree.java
@@ -1,6 +1,9 @@
 
-public class BinaryTreePartThree{
-     static class Node {
+import java.util.ArrayList;
+
+public class BinaryTreePartThree {
+
+    static class Node {
 
         int data;
         Node left;
@@ -18,31 +21,117 @@ public class BinaryTreePartThree{
             return "Node{data=" + data + "}";
         }
     }
+
     //kth level of a tree
-    public static void kthlevel(Node root,int level,int k){
-        if (root==null) {
+    public static void kthlevel(Node root, int level, int k) {
+        if (root == null) {
             return;
         }
-        if (level==k) {
+        if (level == k) {
             System.out.println(root.data);
             return;
         }
-        kthlevel(root.left, level+1, k);
-        kthlevel(root.right, level+1, k);
+        kthlevel(root.left, level + 1, k);
+        kthlevel(root.right, level + 1, k);
     }
+
+    public static boolean getPath(Node root, int n, ArrayList<Node> path) {
+        if (root == null) {
+            return false;
+        }
+
+        path.add(root);
+        if (root.data == n) {
+            return true;
+        }
+        boolean foundLeft = getPath(root.left, n, path);
+        boolean foundRight = getPath(root.right, n, path);
+        if (foundLeft || foundRight) {
+            return true;
+        }
+        path.remove(path.size() - 1); //if not found for left and right child it means it will no be 
+        //be the part of path so remove it 
+        return false;
+    }
+
+    //find LCA - lowest common ancestor solution one
+    public static Node lca(Node root, int n1, int n2) {
+        ArrayList<Node> path1 = new ArrayList<>();
+        ArrayList<Node> path2 = new ArrayList<>();
+        getPath(root, n1, path1);
+        getPath(root, n2, path2);
+        //calculate last common ancestor
+        int i = 0;
+        for (; i < path1.size() && i < path2.size(); i++) {
+            if (path1.get(i) != path2.get(i)) {
+                break;
+            }
+        }
+        //last equal node -> i-1th
+        Node lca = path1.get(i - 1);
+        return lca;
+    }
+
+    //lca solution 2
+    public static Node lca2(Node root, int n1, int n2) {
+        if (root == null || root.data == n1 || root.data == n2) {
+            return root;
+        }
+        Node leftLca = lca2(root.left, n1, n2);
+        Node rightLca = lca2(root.right, n1, n2);
+        //leftLca=val but rightLca==null
+        if (rightLca == null) {
+            return leftLca;
+        }
+        if (leftLca == null) {
+            return rightLca;
+        }
+        return root;
+    }
+
+    public static int lcaDist(Node root, int n) {
+        if (root == null) {
+            return -1;
+        }
+        if (root.data == n) {
+            return 0;
+        }
+        int leftDist = lcaDist(root.left, n);
+        int rightDist = lcaDist(root.right, n);
+        if (leftDist == -1 && rightDist == -1) {
+            return -1;
+        }else if (leftDist==-1) {
+            return rightDist+1;
+        }else{
+            return leftDist+1;
+        }
+    }
+//min dist btw nodes
+
+    public static int minDist(Node root, int n1, int n2) {
+        Node lca = lca2(root, n1, n2);
+        int dist1 = lcaDist(lca, n1);
+        int dist2 = lcaDist(lca, n2);
+        return dist1 + dist2;
+    }
+
     public static void main(String[] args) {
-            //     1
-            //    /  \
-            //    2   3
-            //    /\   /\
-            //    4 5  6 7
-            Node root=new Node(1);
-            root.left=new Node(2);
-            root.right=new Node(3);
-            root.left.left=new Node(4);
-            root.left.right=new Node(5);
-            root.right.left=new Node(6);
-            root.right.right=new Node(7);
-            kthlevel(root,1,3);
-     }
+        //     1
+        //    /  \
+        //    2   3
+        //    /\   /\
+        //    4 5  6 7
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+        //kthlevel(root, 1, 3);
+        int n1 = 4, n2 = 6;
+        System.out.println(lca(root, n1, n2));
+        System.out.println(lca2(root, n1, n2));
+        System.out.println(minDist(root, n1, n2));
+    }
 }
